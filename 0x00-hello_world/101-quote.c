48d269bc23df3f3d39678c27032dd0d6313f0945
Philgyam <philemongyamfi9@gmail.com>
2023-02-16 22:33:55 +0000

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that place of are is useful\" , Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that place of are is useful\" - Dora Kor
+par, 2015-10-19\n", stdout);
 	return (1);
 }
