cef6da3fea3e24987764bbadfdc5eb1cb7dd92e0
Philgyam <philemongyamfi9@gmail.com>
2023-02-16 17:34:06 +0000

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-
-/* A proram that prints a string output*/
-
+/**
+ * main - This program prints wiht put function
+ 
+ */         
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puszzle");
-
-        return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
