09b6dbd1dfcd2ac4702afc9d8a618d5f5369b6aa
Philgyam <philemongyamfi9@gmail.com>
2023-02-16 20:04:46 +0000

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* main - A programme that Prints out data sizes
+* Return: 0 (sucess)
+*/
+int main(void)
+{
+printf("Size of a char: %s byte(s)", sizeof(char)\n);
+printf("Size of an int: %d byte(s)", sizeof(int)\n);
+printf("Size of a long int: %lu byte(s)", sizeof(long int)\n);
+printf("Size of a long long int: %lu byte(s)", sizeof(long long int)\n);
+printf("Size of a float: %f byte(s)", sizeof(float)\n);
+return (0);
+
