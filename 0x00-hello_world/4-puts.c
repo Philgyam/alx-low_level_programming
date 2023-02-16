7715af5021508cf3ab8d4d44e5d93832df75125b
Philgyam <philemongyamfi9@gmail.com>
2023-02-16 17:21:18 +0000

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
+/* A proram that prints a string output*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puszzle);
+	puts(""Programming is like building a multilingual puszzle");
+
+        return 0;
 }
