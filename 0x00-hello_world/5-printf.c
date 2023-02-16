e9a55847984b5c58a220c7f942e353d0de7aa15d
Philgyam <philemongyamfi9@gmail.com>
2023-02-16 18:10:56 +0000

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio>
+/**
+ * main - A program that prints out
+ * Return: 0 (sucess);
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of are,");
+return (0);
+}
