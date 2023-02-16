295fe84d27c4302f56dd8a1219f1c38b2ab53ddb
Philgyam <philemongyamfi9@gmail.com>
2023-02-16 22:29:36 +0000

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fput("and that place of are is useful\" , Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that place of are is useful\" , Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
