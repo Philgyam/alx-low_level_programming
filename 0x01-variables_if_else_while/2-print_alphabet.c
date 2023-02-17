3b47589cb9992ed21ecbc0fc1519f13d871c68f5
Philgyam <philemongyamfi9@gmail.com>
2023-02-17 21:30:35 +0000

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+/**
+ * main - main function is described here
+ * 
+ * Return: 0 (success)
+ */
+
+int main(void)
+{ 
+	char a = 'A';
+	putchar(tolower(a));
+return (0);
+}
