eb794b2c6312759d71612b974b31fc3a18fb50ed
Philgyam <philemongyamfi9@gmail.com>
2023-02-20 16:10:54 +0000

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlb.h>
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	char = 'z';
+	while(z >= 'a')
+	{putchar(z);
+		z--;
+	}
+	putchar('\n');
+return (0);
+}
