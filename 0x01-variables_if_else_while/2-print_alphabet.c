72a93107289e2fd534a41fe5a5dd036b47d3a8de
Philgyam <philemongyamfi9@gmail.com>
2023-02-20 10:24:21 +0000

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,13 +2,14 @@
 #include <ctype.h>
 /**
  * main - main function is described here
- * 
  * Return: 0 (success)
  */
-
 int main(void)
-{ 
-	char a = 'A';
-	putchar(tolower(a));
-return (0);
+{
+	char a = 'a';
+
+	while (a <= 'z')
+		putchar(a);
+	a++;
+	return (0);
 }
