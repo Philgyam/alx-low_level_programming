653c044eb175f95f63a4fd1702c63f30a31fc3ab
Philgyam <philemongyamfi9@gmail.com>
2023-02-20 14:05:13 +0000

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,11 @@ int main(void)
 	a='a';
 
 	while (a <= 'z')
-	{if ((a!='q' && a!= 'e') && a <= 'z')
+	{
+		if ((a!='q' && a!= 'e') && a <= 'z')
 		putchar(a);
-		a++;}
+		a++;
+	}
 		putchar('\n');
 		return (0) ;
 }
