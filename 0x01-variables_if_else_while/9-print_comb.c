7ab926d32396e1ec61c124222ffb80e3157988eb
Philgyam <philemongyamfi9@gmail.com>
2023-02-20 19:07:18 +0000

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,16 @@
  * Return: 0 (success)
  */
 int main(void)
-{
+{ 
 	int a;
 
 	for (a = 0; a < 10; a++)
-	{
 		putchar(a + '0');
-		if (a < '0')
-		{
-			putchar(',');
-			putchar(' ');
-
-		}
+	if(a < 0)
+	{
+		putchar(',');
+		putchar(' ');
 	}
-	
-	return (0);
 }
+putchar('\n')
+return (0);
