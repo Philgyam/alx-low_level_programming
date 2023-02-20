6e72153cfb557ae3edd97968b77a860d5db9441d
Philgyam <philemongyamfi9@gmail.com>
2023-02-20 17:45:14 +0000

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - The main
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < '0')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n);
+	return(0);
+}
