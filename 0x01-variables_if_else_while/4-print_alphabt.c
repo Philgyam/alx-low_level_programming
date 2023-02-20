dfdecbb335d689013af48f8624e46edf6d6794c1
Philgyam <philemongyamfi9@gmail.com>
2023-02-20 14:10:23 +0000

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
- *
+ * main - The mai function goes here
  * Return: 0 (sucess)
  */
 int main(void)
 {
 	char a;
-	a='a';
+
+	a = 'a';
 
 	while (a <= 'z')
 	{
-		if ((a!='q' && a!= 'e') && a <= 'z')
+		if ((a != 'q' && a != 'e') && a <= 'z')
 		putchar(a);
 		a++;
 	}
 		putchar('\n');
-		return (0) ;
+		return (0);
 }
