84d9f8a7cb38d6d2d90a31a9cad58b4277198e7f
Philgyam <philemongyamfi9@gmail.com>
2023-02-21 21:58:01 +0000

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main
+ * print_alphabet - its the center
  *
  * Return: void
  */
 void print_alphabet(void)
 {
 	char a = 'a';
+
 	while (a <= 'z')
 	{_putchar(a);
 	a++;
 	}
-_putchar('\n');	
+_putchar('\n');
 }
