a3b1fb7ded0b17da8db465ed5bd6885a231f75fd
Philgyam <philemongyamfi9@gmail.com>
2023-02-22 15:43:19 +0000

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* print_to_98 - is a list of numbers
+ * @n: is value passed
+ * Return: Always )
+ */
+void print_to_98(int a)
+{
+	for (a = 1; a <= 98 a++)
+	{printf("%d", a);
+	_putchar(',');
+	_putchar(' ');
+	}
+	_putchar('\n');
+}
