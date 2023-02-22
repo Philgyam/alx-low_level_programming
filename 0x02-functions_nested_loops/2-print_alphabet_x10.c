1608f04ab3d13091740323e7130204fbae31bdc8
Philgyam <philemongyamfi9@gmail.com>
2023-02-22 01:18:26 +0000

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * *print_alphabet - void
+ * print_alphabet_x10 -  its not empty
  * Return;
  */
 void print_alphabet_x10(void)
 {
 	char c;
 	int b;
-	
+
 	for (b = 1; b <= 10; b++)
 	{
-	for(c = 'a'; c <= 'z'; c++)
-	{_putchar(c);}
-		_putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{_putchar(c); }
+	_putchar('\n');
 	}
 }
