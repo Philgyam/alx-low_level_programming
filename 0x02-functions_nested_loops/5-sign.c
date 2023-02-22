36dce62ef45706a3a9f75cac8826261f6f31daa8
Philgyam <philemongyamfi9@gmail.com>
2023-02-22 13:19:31 +0000

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - It shows the signs
+ * @n - Its the integer
+ * Return : Always 1 when success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{_putchar('+');
+	return (1);
+	}
+	else
+	if (n = 0)
+	{_putchar('0');
+	return 0;
+	}
+	else 
+	if (n < 0)
+	{_putchar('-');
+	return -1;
+	}
+}
