e5b2a664e9ea228c038cd41a90b0cfa4c599d0af
Philgyam <philemongyamfi9@gmail.com>
2023-02-22 01:44:24 +0000

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,14 @@
 #include "main.h"
 /**
- * main - check the code.
+ * Main
  *
- * Return: Always 0.
+ * Return: always 0
  */
-int _islower(int r)
+int _islower(int c)
 {
-    
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (c >= 97; && c <= 122)
+	{
+		return(1);
+	}
+	return (0);
 }
