0935f0512aa8c70e74e109d76c7119de32737d35
Philgyam <philemongyamfi9@gmail.com>
2023-02-22 14:36:20 +0000

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - The last number
+ * @c: the value to be used
+ * Return: Always succes
+ */
+int print_last_digit(int c)
+{
+	int b;
+	b = n %10;
+
+	if (n < 0)
+	{
+	b= b * -1;
+	}
+	_putchar(b + '0');
+	return (b);
+}
