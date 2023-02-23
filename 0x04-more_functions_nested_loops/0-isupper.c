06623c215c597c71d1f9c51924ffcdfae0944882
Philgyam <philemongyamfi9@gmail.com>
2023-02-23 16:46:57 +0000

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
- * _isupper(int c) - Its the my function
+ * _isupper - Its the my function
  * @c: Is the integer
  * Return: Always success
  */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 98)
-	
-	{return (1);}
-	(return (0);
+
+	{
+	return (1);
+	}
+	return (0);
 }
