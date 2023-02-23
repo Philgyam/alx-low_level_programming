ea8d7b16457781f22c0ff76fbe477ff8bfc655e6
Philgyam <philemongyamfi9@gmail.com>
2023-02-23 17:18:10 +0000

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers- Print everything
+ * return : always success
+ * void: the empty space;
+ */
+int print_numbers(void)
+{
+	int a;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+	}
+}
