3520749f1bd2851008cbb7b2d555dbc318aebb1d
Philgyam <philemongyamfi9@gmail.com>
2023-02-23 18:00:55 +0000

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- *
- *
+ * print_most_numbers- This is the main
  */
 void print_most_numbers(void)
 {
 	int x;
-	
+
 	for (x = 0; x <= 9; x++)
 	{
 		if (x != 2 && x != 4)
