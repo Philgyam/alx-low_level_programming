cdf938778929ba5f7938a8238af4a250fad31100
Philgyam <philemongyamfi9@gmail.com>
2023-02-23 19:23:36 +0000

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *more_numbers - Its the main here
+ * Return : brings back the needful
+ */
+void more_numbers(int n)
+{
+	int x; y;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (y > 9)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
+}
+_putchar ('\n')
+}
