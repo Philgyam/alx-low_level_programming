a862d3b00f4a85c9462465d214be7e55b967bb74
Philgyam <philemongyamfi9@gmail.com>
2023-02-23 15:00:55 +0000

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper(int c) - Its the my function
+ * Return: Always success
+ */
+int _isupper(int c)
+{
+	char a;
+	if ( a == _isupper(a))
+	{return (1);}
+return (0);
+}
