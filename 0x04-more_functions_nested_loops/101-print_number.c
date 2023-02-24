526bbf66d97aace24e7dbb36fdcd67a67fc4d76b
Philgyam <philemongyamfi9@gmail.com>
2023-02-24 01:33:13 +0000

    update

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+lude "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integral variable
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int z;
+
+	z = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		z = -n;
+	}
+	if (z / 10)
+		print_number(z / 10);
+	_putchar((z % 10) + '0');
+}
