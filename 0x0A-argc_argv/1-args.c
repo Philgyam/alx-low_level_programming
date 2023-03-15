694a61da5eb48fdac081192d544c1f6c9e25eb46
philgyam <philemongyamfi9@gmail.com>
2023-03-15 19:13:06 +0000

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/* 
+ * main - contains the enteir program
+ * @argc : Counter for program
+ * @args : argument passed by user
+ * return : returns 0 as value 
+ */
+int main(int argc, char *args[]){
+
+	printf("%d\n", argc);
+	return 0;
+}
