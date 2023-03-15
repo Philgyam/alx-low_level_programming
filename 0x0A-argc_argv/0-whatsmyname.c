7e326799819f31f4804102218f5a0855f3de306b
philgyam <philemongyamfi9@gmail.com>
2023-03-15 18:56:52 +0000

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,18 @@
+#include "main.h"
 #include <stdio.h>
 
-/*
- * main - Its the entirety of function
- * 
- *@argc: argument count
- *@argv: argument array
- *Return : 0 always success
+/**
+ * main - prints the name of the program
+ * @argc: count arguments
+ * @argv: name of array
+ * Return: 0
  */
-int main (int argc, char *argv[]){
-	printf("%s\n", argv[0]);
-	return 0;
-} 
 
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
