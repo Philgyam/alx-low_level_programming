1f30d0e4b4487ca0148a0826b3c7d95a70614a90
philgyam <philemongyamfi9@gmail.com>
2023-03-15 18:51:25 +0000

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Its the entirety of function
+ * 
+ *@argc: argument count
+ *@argv: argument array
+ *Return : 0 always success
+ */
+int main (int argc, char *argv[]){
+	printf("%s\n", argv[0]);
+	return 0;
+} 
+
